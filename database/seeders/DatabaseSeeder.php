e8819bd850a5768f4f84b4093939d918c18850eb
Angelo <angelo.adduci1991@gmail.com>
2021-06-09 18:12:35 +0200

    minor fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call(
             [
-               GameSeeder::class
+               GameSeeder::class,
+               UserSeeder::class
             ]);
     }
 }
