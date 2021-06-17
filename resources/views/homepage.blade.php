cc2defe5b829a516296a756afc69ccd09bc80657
Angelo <angelo.adduci1991@gmail.com>
2021-06-17 12:51:33 +0200

    fix su responsivita e design home

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -21,11 +21,11 @@
     </div>
 
     <div class="row w-100 justify-content-center align-items-center px-2 pb-5 pt-md-5 pe-lg-5">
-        <div class="col-12 offset-md-3 col-md-6 offset-lg-2 col-lg-4 mb-4 mb-md-0 d-flex justify-content-center align-items-center pe-0 home-page-position">
-            <x-_next-match :nextGameInfo="$nextGameInfo" />
-        </div>
-        <div class="col-12 offset-md-3 col-sm-10 col-md-6 offset-lg-0 col-lg-5 mt-3 mt-lg-0 d-flex justify-content-center align-items-center pe-0">
+        <div class="col-12 offset-md-3 col-md-6 offset-lg-2 col-lg-5 mb-4 mb-md-0 d-flex justify-content-center align-items-center pe-0 home-page-position">
             <x-_standings :standing="$standing" />
+        </div>
+        <div class="col-12 offset-md-3 col-sm-10 col-md-6 offset-lg-0 col-lg-4 mt-5 mt-lg-0 d-flex justify-content-center align-items-center pe-0">
+            <x-_next-match :nextGameInfo="$nextGameInfo" />
         </div>
     </div>
 </div>
