fc9abb2992255e4af5ba85d51e0b98dde85eb161
Angelo <angelo.adduci1991@gmail.com>
2021-06-24 17:29:20 +0200

    errore trovato nella funzione di sort

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -229,20 +229,20 @@ class UserController extends Controller
                 return 1;
             }
 
-            if($a['total']= $b['total'] && $a['results']==$b['results'] && $a['scorers']>$b['scorers']){
+            if($a['total']== $b['total'] && $a['results']==$b['results'] && $a['scorers']>$b['scorers']){
                 return 1;
             }
 
-            if($a['total']= $b['total'] && $a['results']==$b['results'] && $a['scorers']==$b['scorers'] && $a['signs']>$b['signs']){
+            if($a['total']== $b['total'] && $a['results']==$b['results'] && $a['scorers']==$b['scorers'] && $a['signs']>$b['signs']){
                 return 1;
             }
 
-            if($a['total']= $b['total'] && $a['results']==$b['results'] && $a['scorers']==$b['scorers'] && $a['signs']==$b['signs'] && $a['final_tot']>$b['final_tot']){
+            if($a['total']== $b['total'] && $a['results']==$b['results'] && $a['scorers']==$b['scorers'] && $a['signs']==$b['signs'] && $a['final_tot']>$b['final_tot']){
                 return 1;
             }
             
             if($a['final_bet'] != 0 && $b['final_bet'] != 0){
-                if($a['total']= $b['total'] && $a['results']==$b['results'] && $a['scorers']==$b['scorers'] && $a['signs']==$b['signs'] && $a['final_tot']>$b['final_tot'] && (new Carbon($a['final_bet']))->gt(new Carbon($b['final_bet']))){
+                if($a['total']== $b['total'] && $a['results']==$b['results'] && $a['scorers']==$b['scorers'] && $a['signs']==$b['signs'] && $a['final_tot']>$b['final_tot'] && (new Carbon($a['final_bet']))->gt(new Carbon($b['final_bet']))){
                     return 1;
                 }
             }
